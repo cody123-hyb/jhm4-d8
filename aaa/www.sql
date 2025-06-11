68ae945653e39546e9bd3bd0b86e2e8719f72c91
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-06-11 14:41:53 +0000

    update

--- aaa/www.sql
+++ aaa/www.sql
@@ -1,23 +1,3 @@
--- Active: 1749652481007@@127.0.0.1@3306@mysql
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `APT2004`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `snacks`
---
-
 CREATE TABLE `snacks` (
   `ID` int(11) NOT NULL,
   `Category` varchar(100) DEFAULT NULL,
